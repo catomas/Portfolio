ac96f67270a632ac71e8e1e810a55334e1a6d214
Juan Camilo Echeverri Salazar <juancami98.11@hotmail.com>
2024-02-27 19:27:36 -0500

    fix footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -16,12 +16,14 @@ interface FooterProps {
 
 export const Footer = ({ footerI18n }: FooterProps) => {
   return (
-    <footer className="max-w-3xl pb-36 mx-auto">
+    <footer className="max-w-3xl pb-36 mx-auto px-4">
       <div className="md:flex md:justify-between">
         <Link href="#home">
-          <h4 className="text-3xl font-bold my-6 md:my-0">JC DEV</h4>
+          <h4 className="text-2xl sm:text-3xl font-bold my-6 md:my-0">
+            JC DEV
+          </h4>
         </Link>
-        <div className="px-2 flex justify-between md:gap-8 items-center">
+        <div className="px-2 flex justify-between md:gap-8 items-center text-xs sm:text-base">
           <Link href="#about_me">{footerI18n.links.aboutMe}</Link>
           <Link href="#experience">{footerI18n.links.experience}</Link>
           <Link href="#services">{footerI18n.links.services}</Link>
